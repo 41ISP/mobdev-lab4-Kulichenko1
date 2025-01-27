0e5ae06035610ba4e18dbaead20a96c33cded886
faqqqke <131741212+faqqqke@users.noreply.github.com>
2025-01-27 06:34:26 +0000

    27.01

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -3,29 +3,37 @@ import Button from "../components/Button";
 import "../shared/api.ts";
 import CatApi from "../shared/api.ts";
 import { useState } from "react";
+import { ICatImage } from "../entities/catFacts/catFacts.rdo.ts";
 
 const FeedPage = () => {
-function MyComponent() {
     const [id, setId] = useState();
     const [url, setUrl] = useState();
     const [width, setWidth] = useState();
     const [height, setHeight] = useState();
-}
-    useEffect(() => {
+    const [images, setImages] = useState<ICatImage[]>([])
 
-        
+    useEffect(() => {
+        const fetchData = async () => {
+            const catsimgs = await CatApi.fetchdata()
+            catsimgs && setImages(catsimgs);
+        }
+        fetchData()
     }, [])
 
     const onClick = () => {
-        console.log(CatApi.fetchdata())
+        console.log()
     }
 
-    return(
+    return (
+        
         <div className="feedform">
             <Button onClick={onClick}></Button>
+        const ListItems = catsimgs.map((catsimgs) => 
+                <li>{catsimgs}</li>
         </div>
+        
     )
-    
+
 }
 
 export default FeedPage;
